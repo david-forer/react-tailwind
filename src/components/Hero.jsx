2a43d04ec90593fa731af1fc64007024159e49dc
David J Forer <david.j.forer@gmail.com>
2021-11-02 23:38:36 +0900

    increased nav size and button link added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Hero = () => {
     return (
@@ -12,7 +13,8 @@ const Hero = () => {
                         </h1>
                         <p className="text-sm leading-5 mt-5 text-gray-700 md:w-8/12 lg:w-11/12 lg:text-left text-center dark:text-gray-300">Start off the new year by hitting the ground running with these greta products for a very special price. A culmination of productivity and luxury bundled together with an amazing deal!</p>
                         <div className="mt-8 w-full flex justify-center lg:justify-start lg:w-auto">
-                            <button className="dark:bg-white dark:hover:bg-gray-300 dark:text-gray-800 md:w-auto w-full px-8 py-4 focus:outline-none border border-gray-800 hover:bg-gray-700 bg-gray-800 text-base focus:ring-2 focus:ring-offset-2 focus:ring-gray-700 leading-none text-gray-50">Explore collection</button>
+                        <Link to={`/products`}>
+                            <button className="dark:bg-white dark:hover:bg-gray-300 dark:text-gray-800 md:w-auto w-full px-8 py-4 focus:outline-none border border-gray-800 hover:bg-gray-700 bg-gray-800 text-base focus:ring-2 focus:ring-offset-2 focus:ring-gray-700 leading-none text-gray-50">Explore collection</button></Link>
                         </div>
                     </div>
                     <div className="lg:w-5/12 lg:ml-10 h-full w-full">
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,11 +25,11 @@ function Navbar() {
           🔄 The Deal Store
         </h1>
         <div>
-          <ul className="flex items-center space-x-10 text-sm">
-            <li><Link to="/" className="text-gray-400 hover:text-gray-100">Home</Link></li>
-            <li><Link to="/about" className="text-gray-400 hover:text-gray-100">About Us</Link></li>
-            {/* <li><Link to="/docs" className="text-gray-400 hover:text-gray-100">Docs</Link></li> */}
-            <li><Link to="/products" className="text-gray-400 hover:text-gray-100">Products</Link></li>
+          <ul className="flex items-center space-x-10 text-lg">
+            <li><Link to="/" className="text-gray-300 font-semibold hover:text-white">Home</Link></li>
+            <li><Link to="/about" className="text-gray-300 font-semibold hover:text-white">About Us</Link></li>
+            {/* <li><Link to="/docs" className="text-gray-300 font-semibold hover:text-white">Docs</Link></li> */}
+            <li><Link to="/products" className="text-gray-300 font-semibold hover:text-white">Products</Link></li>
           </ul>
         </div>
         <div>
