4749a3868db22cfa2478bee44e9b64d2b4b53935
David J Forer <david.j.forer@gmail.com>
2021-11-03 10:31:58 +0900

    added loading to home products

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,37 +1,33 @@
-import React, {useState, useEffect} from 'react'
-import ProductCard from '../components/ProductCard';
-import Highlight from './../components/Highlight';
-import Hero from './../components/Hero';
-import Featured from './../components/Featured';
-import Contact from './../components/Contact';
-// import Loader from './../components/Loader';
+import React, { useState, useEffect } from "react";
+import ProductCard from "../components/ProductCard";
+import Highlight from "./../components/Highlight";
+import Hero from "./../components/Hero";
+import Featured from "./../components/Featured";
+import Contact from "./../components/Contact";
+import Loader from "./../components/Loader";
 
 function Home() {
   const [productData, setProductData] = useState();
+  const [loading, setLoading] = useState(false)
 
-  const url =`https://fakestoreapi.com/products/`
+  const url = `https://fakestoreapi.com/products/`;
 
-  useEffect(async() => {
-    
-    const productData = await fetch(url).then(res=>res.json())
-
-    setProductData(productData)
-
-    
-  }, [url])
+  useEffect(async () => {
+    const productData = await fetch(url).then((res) => res.json());
+    setLoading(true);
+    setProductData(productData);
+  }, [url]);
 
   return (
     <div>
-      
       <Hero />
-      {/* if(product.loading){
-        <Loader />
-      } */}
-      { productData && <Featured productInfo={productData} />}
+
+      {/* { productData && <Featured productInfo={productData} />} */}
+      {loading ? ( productData && <Featured productInfo={productData} /> ) : <Loader />}
       <Highlight />
       <Contact />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,14 +1,15 @@
 import React, {useState, useEffect} from 'react'
 import ProductCard from '../components/ProductCard';
-// import Loader from '../components/Loader';
+import Loader from '../components/Loader';
 // import {Link} from 'react-router-dom'
 
 const Products = () => {
   const [products, setProducts] = useState();
+  const [loading, setLoading] = useState(false)
 
   useEffect(async() => {
     const products = await fetch('https://fakestoreapi.com/products/').then(res=>res.json())
-
+    setLoading(true)
     setProducts(products)
 console.log(products)
     
@@ -17,7 +18,8 @@ console.log(products)
   return (
     <div>
     <h1 className="text-center text-6xl font-bold mb-24 mt-10">Our Current Products</h1>
-       { products && <ProductCard productInfo={products} />}
+       {/* { products && <ProductCard productInfo={products} />} */}
+       {loading ? ( products && <ProductCard productInfo={products} /> ) : <Loader />}
     </div>
   )
 }
